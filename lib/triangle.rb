d78ec619263b7646cd12ea98df06f72b57951d94
Andy <wang.andy32@gmail.com>
2018-09-02 23:00:37 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
 class Triangle
-  # write code here
+  def initialize(l1,l2,l3)
+    @l1 = l1
+    @l2 = l2
+    @l3 = l3
+    @sides = [@l1,@l2,@l3].sort
+  end
+  def kind
+    if @sides.any? do |side|
+      side <= 0
+    end
+    raise TriangleError
+    end
+    if @sides[0] + @sides[1] <= @sides[2]
+      raise TriangleError
+    end
+
+    case @sides.uniq.size
+      when 1 then :equilateral
+      when 2 then :isosceles
+      else        :scalene
+    end
+  end
+end
+
+class TriangleError < StandardError
 end
